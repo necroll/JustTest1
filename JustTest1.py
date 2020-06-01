33b5aa54a5720878c02f0be52a067c2a7b0162fc
necroll <66201905+necroll@users.noreply.github.com>
2020-06-01 12:13:08 +0300

    Update JustTest1.py

--- JustTest1.py
+++ JustTest1.py
@@ -2,13 +2,16 @@ import telebot
 import random
 import datetime
 from datetime import datetime
+from pytz import timezone
 
 bot = telebot.TeleBot('1019968029:AAEdaVL7zgsYihvbMcr4bCeL3hrZqrjM3MM')
+
+
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Стартуй нахуй!')
-    bot.send_message(message.chat.id, datetime.now())
+    bot.send_message(message.chat.id, datetime.now(timezone('Europe/Moscow')))
 
 
 
